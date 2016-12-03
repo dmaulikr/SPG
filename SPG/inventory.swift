b41e85b1ffdc298f47cb8229dd14ebe78bf44574
Student <student@students-macbook-pro.local>
2016-12-02 19:41:35 -0500

    took away some test items

--- SPG/inventory.swift
+++ SPG/inventory.swift
@@ -21,19 +21,11 @@ class Inventory {
         let tempItem = Items()
         let tempItem1 = Items()
         let tempItem2 = Items()
-        let tempItem3 = Items()
-        let tempItem4 = Items()
-        let tempItem5 = Items()
-        let tempItem6 = Items()
-        let tempItem7 = Items()
+
         itemList.append(tempItem)
         itemList.append(tempItem1)
         itemList.append(tempItem2)
-        itemList.append(tempItem3)
-        itemList.append(tempItem4)
-        itemList.append(tempItem5)
-        itemList.append(tempItem6)
-        itemList.append(tempItem7)
+   
         
 
 
